2fd3916a03763541a68186cd9c44f03fe2faa418
Leo Singer <leo.singer@ligo.org>
2020-07-15 15:15:42 -0400

    Run unit tests for mysql

--- healpix_alchemy/tests/conftest.py
+++ healpix_alchemy/tests/conftest.py
@@ -1,20 +1,22 @@
 import pytest
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 
-@pytest.fixture
-def engine(postgresql):
-    """Create an SQLAlchemy engine with a disposable PostgreSQL database."""
-    return create_engine('postgresql://',
+@pytest.fixture(params=['mysql', 'postgresql'])
+def engine(request):
+    """Parametrize SQLAlchemy engines over SQLite and PostgreSQL."""
+    connection = request.getfixturevalue(request.param)
+    return create_engine(URL(request.param),
                          poolclass=StaticPool,
-                         creator=lambda: postgresql)
+                         creator=lambda: connection)
 
 
 @pytest.fixture
 def session(engine):
-    """Create an SQLAlchemy session with a disposable PostgreSQL database."""
+    """Create an SQLAlchemy session with a disposable database."""
     Session = sessionmaker(bind=engine)
     session = Session()
     yield session
