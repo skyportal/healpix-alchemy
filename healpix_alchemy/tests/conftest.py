5ac54021405fdf4262fe053b3438a1289d43d7b5
Leo Singer <leo.singer@ligo.org>
2021-08-19 20:04:56 -0400

    Report database size

--- healpix_alchemy/tests/conftest.py
+++ healpix_alchemy/tests/conftest.py
@@ -13,9 +13,32 @@ def engine(postgresql):
 
 
 @pytest.fixture
-def session(engine):
+def session(engine, record_database_size):
     """Create an SQLAlchemy session with a disposable PostgreSQL database."""
     Session = sessionmaker(bind=engine)
     session = Session()
     yield session
+    record_database_size()
     session.close()
+
+
+@pytest.fixture
+def record_database_size(record_property, engine):
+
+    def func():
+        (database_size,), = engine.execute(
+            'select pg_size_pretty(pg_database_size(current_database()))')
+        record_property('database_size', database_size)
+
+    return func
+
+
+def pytest_terminal_summary(terminalreporter):
+    terminalreporter.section('Database size')
+    for report in terminalreporter.getreports(''):
+        try:
+            database_size = dict(report.user_properties)['database_size']
+        except KeyError:
+            pass
+        else:
+            terminalreporter.line(f'{report.nodeid}: {database_size}')
