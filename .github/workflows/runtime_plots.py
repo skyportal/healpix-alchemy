5af75f9ead8012e2bd3966cc92ed6982d40f62fe
bparazin <bparazin1225@gmail.com>
2021-10-26 16:06:15 -0500

    Running a ls command in workflows to see what directory of the project it is done in and maybe if output.json is there

--- .github/workflows/runtime_plots.py
+++ .github/workflows/runtime_plots.py
@@ -25,8 +25,8 @@ def runtime_plot(benchmark_list):
     ax.set_ylabel('Runtime (s)')
     ax.set_yscale('log')
     ax.set_xscale('log')
-    ax.set_title(f'{test} Runtime Plot')
-    plt.savefig(f'{test} runtime plot')
+    ax.set_title(test + ' Runtime Plot')
+    plt.savefig(test + ' runtime plot')
 
 benchmark_list = data_list_to_dict(data)
 for test in benchmark_list:
