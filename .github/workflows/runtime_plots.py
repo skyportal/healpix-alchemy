114ffcbe8c7274f06a2b6d22e2daec7d01c78220
bparazin <bparazin1225@gmail.com>
2021-11-03 15:57:55 -0500

    Moved changes to this branch which isn't broken?

---
+++ .github/workflows/runtime_plots.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import json
+import pandas
+
+with open('benchmark_results.json') as f:
+    data = json.load(f)
+
+def data_list_to_dict(data):
+    result_dict = {}
+    for test_run in data['benchmarks']:
+        test = test_run['fullname'].split('[')[0]
+        if test in result_dict.keys():
+            result_dict[test].append(test_run)
+        else:
+            result_dict[test] = [test_run]
+    return result_dict
+
+def runtime_plot(benchmark_list):
+    N_list = [int(run['param']) for run in benchmark_list]
+    t_list = [float(run['stats']['mean']) for run in benchmark_list]
+    t_err = [float(run['stats']['stddev']) for run in benchmark_list]
+    test = benchmark_list[0]['fullname'].split('[')[0].split('::')[1]
+    fig, ax = plt.subplots()
+    ax.errorbar(N_list, t_list, yerr = t_err, fmt = 'o')
+    ax.set_xlabel('N')
+    ax.set_ylabel('Runtime (s)')
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_title(test + ' Runtime Plot')
+    plt.savefig('plots/' + test + ' runtime plot')
+
+benchmark_list = data_list_to_dict(data)
+for test in benchmark_list:
+    runtime_plot(benchmark_list[test])
