812d9e787955a2df914fc6df2f8e382aad81118d
bparazin <bparazin1225@gmail.com>
2021-11-04 13:31:01 -0500

    wrap to less than 80 char per line

--- .github/workflows/runtime_plots.py
+++ .github/workflows/runtime_plots.py
@@ -12,6 +12,7 @@ df['fullfunc'] = df['fullname'].str.split('[', expand=True)[0]
 df['param'] = pd.to_numeric(df['param'])
 for func, group in df.groupby('func'):
     fig, ax = plt.subplots()
-    group.plot.scatter(x='param', y='stats.mean', yerr='stats.stddev', loglog=True, xlabel='N', ylabel='Runtime (s)', ax=ax)
+    group.plot.scatter(x='param', y='stats.mean', yerr='stats.stddev',
+                       loglog=True, xlabel='N', ylabel='Runtime (s)', ax=ax)
     fig.tight_layout()
     fig.savefig(f'plots/{func}')
