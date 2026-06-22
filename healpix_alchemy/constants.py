e8a6f800507dc643c2373b55573565af24928e2d
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-06-22 08:57:07 -0500

    overflow issue

--- healpix_alchemy/constants.py
+++ healpix_alchemy/constants.py
@@ -4,9 +4,13 @@ from astropy.coordinates import ICRS
 from astropy_healpix import HEALPix, level_to_nside
 from mocpy import MOC
 
-LEVEL = MOC.MAX_ORDER
+LEVEL = int(MOC.MAX_ORDER)
 """Base HEALPix resolution. This is the maximum HEALPix level that can be
-stored in a signed 8-byte integer data type."""
+stored in a signed 8-byte integer data type.
+
+Coerced to a Python :class:`int` because newer versions of mocpy return
+:class:`numpy.uint8`, which would silently overflow in expressions like
+``4**LEVEL``."""
 
 HPX = HEALPix(nside=level_to_nside(LEVEL), order="nested", frame=ICRS())
 """HEALPix projection object."""
