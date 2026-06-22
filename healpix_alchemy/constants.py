0ac5e84801fd45190c3026ec91b65ddc77edd6d6
Michael Coughlin <michael.w.coughlin@gmail.com>
2026-06-22 10:12:34 -0500

    Leo comment

--- healpix_alchemy/constants.py
+++ healpix_alchemy/constants.py
@@ -4,13 +4,11 @@ from astropy.coordinates import ICRS
 from astropy_healpix import HEALPix, level_to_nside
 from mocpy import MOC
 
+# Coerce to a Python int because newer versions of mocpy return a numpy.uint8,
+# which would silently overflow in expressions like 4**LEVEL.
 LEVEL = int(MOC.MAX_ORDER)
 """Base HEALPix resolution. This is the maximum HEALPix level that can be
-stored in a signed 8-byte integer data type.
-
-Coerced to a Python :class:`int` because newer versions of mocpy return
-:class:`numpy.uint8`, which would silently overflow in expressions like
-``4**LEVEL``."""
+stored in a signed 8-byte integer data type."""
 
 HPX = HEALPix(nside=level_to_nside(LEVEL), order="nested", frame=ICRS())
 """HEALPix projection object."""
